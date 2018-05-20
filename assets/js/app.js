45047dcccf4e3a0e4c409e901470a9f2879f2230
casee8 <pokerkaszy7@gmail.com>
2018-05-20 20:40:40 +0100

    Lots of changes in HTML, CSS (reworked CSS Grid) and JS (organized with plenty of deletions)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,16 @@
-var locationName = document.getElementsByClassName("locationName")[0];
-var description = document.querySelector(".description");
-var temperat = document.getElementById("temperat");
-var img = document.querySelector("img");
+var loc    = document.querySelector("#location"),
+    weatherpic  = document.querySelector("#weatherPic"),
+    temp        = document.querySelector("#temp"),
+    precip      = document.querySelector("#precip"),
+    humidity    = document.querySelector("#humid"),
+    wind        = document.querySelector("#wind");
+
+// =====================================================================================
+// GETTING THE USER'S LOCATION
+// =====================================================================================
+
 var lat;
 var lon;
-// More Info staff
-var button = document.querySelector("a"),
-    moreInfo = document.querySelector(".moreInfo"),
-    tempMinPlace = document.querySelector("#tempMin"),
-    tempMaxPlace = document.querySelector("#tempMax"),
-    humidityPlace = document.querySelector("#humidity"),
-    pressurePlace = document.querySelector("#pressure"),
-    windSpeedPlace = document.querySelector("#windSpeed");
 
 function getLocation() {
   if (navigator.geolocation) {
@@ -28,6 +27,24 @@ function showPosition(position) {
   weatherApp();
 }
 
+function geoCoding() {
+  var xmlhttp = new XMLHttpRequest();
+  var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lon + "&key=AIzaSyDAHgPXl5edvHYkxvsNb8YMvVdNHZfF0io";
+  xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var mySecArr = JSON.parse(this.responseText);
+      var nearLocation = mySecArr.results[4].formatted_address;
+      loc.innerText = nearLocation;
+    }
+  }
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+}
+
+// =====================================================================================
+// WEATHER API - DATA STORING
+// =====================================================================================
+
 function weatherApp() {
   var xmlhttp = new XMLHttpRequest();
   var url = "https://api.wunderground.com/api/b47388355b8e168a/conditions/q/" + lat + "," + lon + ".json";
@@ -36,7 +53,7 @@ function weatherApp() {
       var myArr = JSON.parse(this.responseText);
       var data = myArr.current_observation;
       // ============================================
-      // STORING WEATHER DATA
+      // Storing weather data
       // ============================================
       var tempC       = data.temp_c,
           tempF       = data.temp_f,
@@ -49,7 +66,7 @@ function weatherApp() {
           wUBrandImg  = data.image.url,
           timeStamp   = data.observation_time_rfc822,
           precip1Hr   = data.precip_1hr_string,
-          precipToday = data.pecip_today_string,
+          precipToday = data.precip_today_string,
           pressureIn  = data.pressure_in,
           pressureMb  = data.pressure_mb,
           relHumidity = data.relative_humidity,
@@ -64,40 +81,13 @@ function weatherApp() {
           windKph     = data.wind_kph,
           windMph     = data.wind_mph;
 
-
-
-
-
-      var temperature = myArr.current_observation.temp_c;
-      var temperatureF = myArr.current_observation.temp_c;
-      var weatherCondition = myArr.current_observation.weather;
-      var icon = myArr.current_observation.icon_url;
-
-      //         MORE INFO
-      var tempMin = myArr.current_observation.dewpoint_c,
-          // tempMax = myArr.current_observation.main.temp_max,
-          humidity = myArr.current_observation.relative_humidity,
-          pressure = myArr.current_observation.pressure_mb,
-          windSpeed = myArr.current_observation.wind_kph;
-
-      temperat.innerText = temperature;
-      description.innerText = weatherCondition;
-      img.setAttribute("src", icon);
-      // tempToggle.onclick = function () {
-      //   if (tempToggle.innerText == "C") {
-      //     tempToggle.innerText = "F";
-      //     temperat.innerText = Math.round(temperature * (9 / 5) + 32);
-      //   } else {
-      //     tempToggle.innerText = "C";
-      //     temperat.innerText = Math.round(temperature);
-      //   }
-      // }
-
-      tempMinPlace.innerHTML = "Minimum temperature: " + tempMin + " °<span class='tempToggle'>C</span>";
-      // tempMaxPlace.innerHTML = "Maximum temperature: " + tempMax + " °<span class='tempToggle'>C</span>";
-      humidityPlace.innerText = "Humidity: " + humidity;
-      pressurePlace.innerText = "Pressure: " + pressure + " mBar";
-      windSpeedPlace.innerText = "Wind: " + windSpeed + " km/h";
+      // ============================================
+      // ShowTime
+      // ============================================
+      temp.innerText = tempC;
+      precip.innerText = precipToday;
+      humidity.innerText = relHumidity;
+      wind.innerText = windKph;
 
       button.onclick = function () {
         $(moreInfo).toggleClass("hideMoreInfo");
@@ -107,18 +97,4 @@ function weatherApp() {
   xmlhttp.open("GET", url, true);
   xmlhttp.send();
   geoCoding();
-}
-
-function geoCoding() {
-  var xmlhttp = new XMLHttpRequest();
-  var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lon + "&key=AIzaSyDAHgPXl5edvHYkxvsNb8YMvVdNHZfF0io";
-  xmlhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      var mySecArr = JSON.parse(this.responseText);
-      var nearLocation = mySecArr.results[4].formatted_address;
-      locationName.innerHTML = "<h2>" + nearLocation + "</h2>";
-    }
-  }
-  xmlhttp.open("GET", url, true);
-  xmlhttp.send();
 }
